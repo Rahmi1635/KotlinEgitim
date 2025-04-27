7d741f6f72486ad8ee2536c45dd4c7f8d8ff5e54
Rahmi1635 <rahmiGoktas1635@gmail.com>
2025-04-27 18:46:28 +0300

    Kotlin' de değişken tanımlama ve değer atama
     var : (variable) yani oluşturulduktan sonra değişkenin değeri değiştirilebilir.Dinamiktir
    val : (value) yani değişken oluşturulduktan sonra değeri değiştirilemez . Statiktir

---
+++ ValueAssignment.kt
@@ -0,0 +1,111 @@
+package com.example.firstapp
+
+fun main() {
+    // FIRST ROW
+
+    var city="Bursa"
+    val country="Rusya"
+    var phoneNumber="03126891578"
+    val mailCode=15896
+    var job="Yazılım Mühendisi"
+    val stockAmount=500
+    var customerName="Levent"
+    val balance=62000
+    var birthdate="25/02/1995"
+    val salary=70000
+    var maritalStatus="Bekar"
+    val productComment="İyi"
+
+    println("Şehir : $city")
+    println("Ülke : $country")
+    println("Telefon Numarası : $phoneNumber")
+    println("Posta Kodu : $mailCode")
+    println("Meslek : $job")
+    println("Stok Adedi : $stockAmount")
+    println("Müşteri Adı : $customerName")
+    println("Bakiye : $balance")
+    println("Doğum Tarihi : $birthdate")
+    println("Maaş : $salary")
+    println("Medeeni Hali : $maritalStatus")
+    println("Ürün Yorumu : $productComment")
+
+    // SECOND ROW
+
+    var paymentDate="05/06/2025"
+    val payment=15000
+    var orderAmount=100
+    val carModel="Mercedes S Serisi"
+    var bookName="Nutuk"
+    var publishDate="09/12/2001"
+    val salesAmount=25
+    var roomAmount=5
+    val latitude="36-42 Kuzey Enlemleri"
+    var longitude="26-45 Doğu Meridyenleri"
+    val productName="LG Televizyon"
+        var foodPrice=300
+
+    println("Ödeme Tarihi : $paymentDate")
+    println("Ödeme : $payment")
+    println("Sipariş Adedi : $orderAmount")
+    println("Araba Modeli : $carModel")
+    println("Kitap Adı : $bookName")
+    println("Yayınlanma Tarihi : $publishDate")
+    println("Satış Adedi : $salesAmount")
+    println("Oda Sayısı : $roomAmount")
+    println("Enlem : $latitude")
+    println("Boylam : $longitude")
+    println("Ürün Adı : $productName")
+    println("Yemek Fiyatı : $foodPrice")
+
+    // THIRD ROW
+
+    val brand = "Volvo"
+    var musicName="Zalim"
+    val videoDuration=3
+    var productPoint=8.5
+    val imageName="Manzara"
+    var fileFormat="jpg"
+    val color="Mavi"
+    var colorCode="255.156.154"
+    val phoneModel="A modeli"
+    val screenDimension=6.6
+    var weight=2
+
+    println("Marka : $brand")
+    println("Müzik İsmi  : $musicName")
+    println("Video Süresi : $videoDuration")
+    println("Ürün Puanı : $productPoint")
+    println("Resim Adı : $imageName")
+    println("Dosya Formatı : $fileFormat")
+    println("Renk : $color")
+    println("Renk Kodu : $colorCode")
+    println("Telefon Modeli : $phoneModel")
+    println("Ekran Boyutu : $screenDimension")
+    println("Ağırlık: $weight")
+
+    // FOURD ROW
+
+    val nationalDay = "29 Ekim"
+    var offDay="Pazar"
+    val bookingDate="12/05/2025"
+    var streetName="Altay"
+    val busLine="8L"
+    var restMinute=15
+    val followCode="A85DSAF62JK"
+    var ticketDuration=5
+    val ticketCode="KRLSM85SDF74563"
+    var billAddress="Gölbaşı"
+
+
+    println("Ulusal Gün : $nationalDay")
+    println("Tatil Günü : $offDay")
+    println("Rezervasyon Tarihi : $bookingDate")
+    println("Sokak Adı : $streetName")
+    println("Otobüs Hattı : $busLine")
+    println("Kalan Dakika : $restMinute")
+    println("Takip Kodu : $followCode")
+    println("Kupon Süresi : $ticketDuration")
+    println("Kupon Kodu : $ticketCode")
+    println("Fatura Adresi : $billAddress")
+
+}
